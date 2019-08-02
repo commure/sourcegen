d50428ced06a65650f55c5a8b9a95e4ac1fb219b
Ivan Dubrov <ivan@commure.com>
2019-08-02 08:51:11 -0700

    Starting working on a test suite for the generator.
    Fixes #6

---
+++ cli/tests/suite.rs
@@ -0,0 +1,41 @@
+use failure::Error;
+use sourcegen_cli::SourcegenParameters;
+use std::path::Path;
+
+pub mod generators;
+pub mod helpers;
+
+fn main() -> Result<(), Error> {
+    let temp = tempfile::tempdir()?;
+    let root = temp.path().join("root");
+    copy_dir::copy_dir("tests/test_data", &root)?;
+    for entry in std::fs::read_dir(root)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir()
+            && path
+                .file_name()
+                .map_or(true, |name| name != "fake_sourcegen")
+        {
+            run_test_dir(&path)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn parameters(manifest: &Path) -> SourcegenParameters {
+    SourcegenParameters {
+        manifest: Some(manifest),
+        generators: &[("write-back", &self::generators::WriteBack)],
+        ..Default::default()
+    }
+}
+
+fn run_test_dir(dir: &Path) -> Result<(), failure::Error> {
+    let manifest = dir.join("input").join("Cargo.toml");
+    sourcegen_cli::run_sourcegen(&parameters(&manifest))?;
+
+    self::helpers::assert_matches_expected(dir, &dir.join("input"), &dir.join("expected"))?;
+    Ok(())
+}
---
+++ cli/tests/test_data/001-strip-attributes/input/src/lib.rs
@@ -0,0 +1,7 @@
+#[another]
+#[sourcegen::sourcegen(generator = "write-back")]
+// Generated. All manual edits to the block annotated with #[sourcegen...] will be discarded.
+#[derive(Custom)]
+struct Hello {
+    field: usize,
+}
