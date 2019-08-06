02a28e905c1aeb674bf6ec833771062e2b971f87
Ivan Dubrov <ivan@commure.com>
2019-08-05 22:11:28 -0700

    Alternative way for full file sourcegen.

---
+++ cli/tests/test_data/006-complete-file-workaround/expected/src/lib.rs
@@ -0,0 +1,8 @@
+#[sourcegen::sourcegen(generator = "generate-file", file = true)]
+struct __Unused;
+
+// Generated. All manual edits below this line will be discarded.
+#[doc = r" Some generated comment here"]
+struct Hello {
+    pub hello: String,
+}
